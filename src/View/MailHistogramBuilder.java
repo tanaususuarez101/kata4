f2ebebfd5d5428f9fcabbe5cd05f162e5454a51a
Tana - HP <tanausu.suarez101@alu.ulpgc.es>
2017-10-27 23:44:30 +0100

    Version 1.0

--- src/View/MailHistogramBuilder.java
+++ src/View/MailHistogramBuilder.java
@@ -1,4 +1,13 @@
 package View;
+import Model.*;
+import java.util.*;
 
 public class MailHistogramBuilder {
+    public static Histogram<String> build(List<String>mail){
+        Histogram<String> histo = new Histogram<>();
+        for (String mail1: histo.keySet() ) {
+            histo.Increment(new Mail(mail1).getDomain());
+        }
+        return histo;
+    }
 }
