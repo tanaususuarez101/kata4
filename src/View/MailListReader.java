bcc496506c5c60c1ab4540e2720a4e4275b66a80
Tana <tanausu.suarez@alu.ulpgc.es>
2017-10-28 13:36:30 +0100

    Versión 1 - fin de la clase MailListReader

---
+++ src/View/MailListReader.java
@@ -0,0 +1,19 @@
+package View;
+import java.util.*;
+import java.io.*;
+
+public class MailListReader{
+
+    public static List<String> read(String name){
+        List<String> mailList = new ArrayList<>();;
+        String mail;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(name)));
+            while ((mail = reader.readLine()) != null) if(mail.contains("@")) mailList.add(mail);
+            reader.close();
+        }
+        catch(FileNotFoundException a){System.out.println("Fichero no encontrado "+a);}
+        catch (IOException a){System.out.println("Error entrada y salida");}
+        return mailList;
+    }
+}
